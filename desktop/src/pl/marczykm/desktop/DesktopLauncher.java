8ca22f445da4ca561625ab86907a9584577521c7
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-10-12 09:03:59 +0200

    initialized repository

---
+++ desktop/src/pl/marczykm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.marczykm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.marczykm.DayAtTheOffice;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DayAtTheOffice(), config);
+	}
+}
---
+++ html/src/pl/marczykm/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package pl.marczykm.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import pl.marczykm.DayAtTheOffice;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new DayAtTheOffice();
+        }
+}
